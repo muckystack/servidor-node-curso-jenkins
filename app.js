ed2e025b3a58cd6f224a0960f563ec46d2cc5f81
Miguel Angel Martinez Puga <muckystack@gmail.com>
2024-07-09 19:42:15 -0500

    Servidor de node

--- app.js
+++ app.js
@@ -10,5 +10,6 @@ app.use('/', indexRouter);
 app.listen(port, () => {
     console.log(`Servidor escuchando en http://localhost:${port}`);
     console.log("Bienvenido seas a la aplicacion de prueba del servicio de jenkins")
+    console.log("Se agrego un cambio")
 });
 
